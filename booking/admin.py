1a9b9531962dbd2eb18413647ddacbad10cdfc53
Brian Magaña <111398568+BrialexMaga@users.noreply.github.com>
2026-02-04 21:59:02 -0600

    Add model forms to Django's admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Booking, BookingDetail, ScreeningSeat, Movie, Room, Seat, TypeSeat, Screening
 
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(BookingDetail)
+admin.site.register(ScreeningSeat)
+admin.site.register(Movie)
+admin.site.register(Room)
+admin.site.register(Seat)
+admin.site.register(TypeSeat)
+admin.site.register(Screening)
